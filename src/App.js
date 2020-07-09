91765815a9db761290663d305a060d0c30c310eb
vikas62081 <vikas62081@gmail.com>
2020-07-09 17:07:52 +0530

    adding promise all code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
-import React ,{useEffect,useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import Backdrop from '@material-ui/core/Backdrop';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
+import axios from 'axios';
 
-const useStyles = makeStyles((theme) => ({
-  backdrop: {
-    zIndex: theme.zIndex.drawer + 1,
-    color: '#fff',
-  },
-}));
 function App() {
-  const classes=useStyles();
-  const [post,setPost]=useState(null)
-  useEffect(()=>{
-    fetch("https://jsonplaceholder.typicode.com/posts/1")
-    .then(response=>response.json())
-    .then(response=>{
-      const {body}=response
-      setPost(body)
-    })
-  },[])
+  const [users, setUsers] = useState()
+  useEffect(() => {
+    axios.all([
+      axios.get("https://jsonplaceholder.typicode.com/posts"),
+      axios.get("https://jsonplaceholder.typicode.com/users")
+    ])
+      .then(([posts, users]) => {
+        console.log(posts)
+        setUsers(users.data)
+      }
+      )
+      .catch(err => console.log(err))
+  }, [])
   return (
     <div className="App">
-      <h1 align="center">React-App</h1> 
-      {post?post :<Backdrop className={classes.backdrop} open>
-        <CircularProgress color="inherit" />
-      </Backdrop>}
-      
+      <h1 align="center">React-App</h1>
+      {users && users.map(user => <h4>{user.name}</h4>)}
     </div>
   );
 }
