a78c5f239c00071ef51966643f6ed5fd9b5399ec
vikas62081 <vikas62081@gmail.com>
2020-07-10 12:30:04 +0530

    'Simply'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
-import axios from 'axios';
 
 function App() {
-  const [users, setUsers] = useState()
-  useEffect(() => {
-    axios.all([
-      axios.get("https://jsonplaceholder.typicode.com/posts"),
-      axios.get("https://jsonplaceholder.typicode.com/users")
-    ])
-      .then(([posts, users]) => {
-        console.log(posts)
-        setUsers(users.data)
-      }
-      )
-      .catch(err => console.log(err))
-  }, [])
+
   return (
     <div className="App">
       <h1 align="center">React-App</h1>
-      {users && users.map(user => <h4>{user.name}</h4>)}
+      
     </div>
   );
 }
