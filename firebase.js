f01ecda2aa12d53a73c2d9253855cb00fe353c83
vikas62081 <vikas62081@gmail.com>
2020-07-19 20:36:01 +0530

    uploaded

--- firebase.js
+++ firebase.js
@@ -1,13 +1,6 @@
 const firebase=require('firebase')
 var firebaseConfig = {
-    apiKey: "AIzaSyB81weo1DEyW-fsIQW8-vig8kkY14aufAY",
-    authDomain: "my-app-bf141.firebaseapp.com",
-    databaseURL: "https://my-app-bf141.firebaseio.com",
-    projectId: "my-app-bf141",
-    storageBucket: "my-app-bf141.appspot.com",
-    messagingSenderId: "145964966489",
-    appId: "1:145964966489:web:d867a03481b2a173a88748",
-    measurementId: "G-WGKZNG82MK"
+  //  provide your firebaseConfig
   };
   // Initialize Firebase
  const db= firebase.initializeApp(firebaseConfig).firestore();
