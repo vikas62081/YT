002ff8ad278f2557fe0d8b19c034496ca05b1d5b
vikas62081 <vikas62081@gmail.com>
2020-08-29 13:28:45 +0530

    ag grid custom cell renderer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import './App.css';
+import { AgGridReact } from 'ag-grid-react';
 
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 function App() {
+  const actionButton=(params)=>{
+    console.log(params);
+    alert(`${params.data.make} ${params.value}`)
+  }
+  const columnDefs= [
+    { headerName: "Make", field: "make" },
+    { headerName: "Model", field: "model" },
+    { headerName: "Price", field: "price" },
+    {headerName: "Action",field:"price",
+    cellRendererFramework:(params)=><div>
+      <button onClick={()=>actionButton(params)}>Click me</button>
+    </div>}
+    ]
+  const rowData= [
+    { make: "Toyota", model: "Celica", price: 35000 }, 
+    { make: "Ford", model: "Mondeo", price: 32000 }, 
+    { make: "Porsche", model: "Boxter", price: 72000 }]
 
   return (
     <div className="App">
       <h1 align="center">React-App</h1>
-      
+      <div className="ag-theme-alpine" style={ {height: '200px'} }>
+        <AgGridReact
+            columnDefs={columnDefs}
+            rowData={rowData}
+            defaultColDef={{flex:1}}>
+        </AgGridReact>
+      </div>
     </div>
   );
 }
