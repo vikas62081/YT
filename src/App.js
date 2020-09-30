cd64fb0f0ed4dc87715b5f3b4353ad8185faa119
vikas62081 <vikas62081@gmail.com>
2020-09-30 21:37:25 +0530

    adding filter aand export

--- src/App.js
+++ src/App.js
@@ -5,13 +5,10 @@ import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 function App() {
-
+let gridApi
   const columnDefs= [
     { headerName: "Name", field: "name" },
-    { headerName: "Age", field: "age",
-    // cellStyle:(params)=>(params.value>18?{borderLeft:"4px green solid"}:{borderLeft:"4px red solid"})
-    cellClass:(params)=>(params.value>18?"moreThan18":"lessThan18")
-  }, 
+    { headerName: "Age", field: "age",}, 
     {headerName: "Birth Year",field: "birthYear",},
     { headerName: "Phone Number", field: "phoneNumber" },
    
@@ -20,16 +17,24 @@ function App() {
     { name: "Rahul", age: 19, phoneNumber: 9876543210, birthYear:2001}, 
     { name: "David", age: 17, phoneNumber: 9827654310,birthYear:2003}, 
     { name: "Dan", age: 25, phoneNumber: 9765438210,birthYear:1995 }]
-
+const defaultColDef={sortable:true,editable:true,flex:1,filter:true,floatingFilter:true}
+const onGridReady=(params)=>{
+  gridApi=params.api
+}
+const onExportClick=()=>{
+  gridApi.exportDataAsCsv();
+}
   return (
     <div className="App">
       <h1 align="center">React-App</h1>
       <h3>Student Details</h3>
+      <button onClick={()=>onExportClick()}>Export</button>
       <div className="ag-theme-alpine" style={ {height: '200px'} }>
         <AgGridReact
             columnDefs={columnDefs}
             rowData={rowData}
-            defaultColDef={{flex:1,minWidth:100}}>
+            defaultColDef={defaultColDef}
+            onGridReady={onGridReady}>
         </AgGridReact>
       </div>
     </div>
