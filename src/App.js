ded64aed31cd0561313f42a99119b704b32be5c7
vikas62081 <vikas62081@gmail.com>
2020-10-02 13:21:08 +0530

    initial commit for data from external API

--- src/App.js
+++ src/App.js
@@ -5,11 +5,10 @@ import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 function App() {
-let gridApi
   const columnDefs= [
     { headerName: "Name", field: "name" },
-    { headerName: "Age", field: "age",}, 
-    {headerName: "Birth Year",field: "birthYear",},
+    { headerName: "Age", field: "age",tooltipField: 'name',}, 
+    {headerName: "Birth Year",field: "birthYear", tooltipField: 'name',},
     { headerName: "Phone Number", field: "phoneNumber" },
    
     ]
@@ -17,24 +16,25 @@ let gridApi
     { name: "Rahul", age: 19, phoneNumber: 9876543210, birthYear:2001}, 
     { name: "David", age: 17, phoneNumber: 9827654310,birthYear:2003}, 
     { name: "Dan", age: 25, phoneNumber: 9765438210,birthYear:1995 }]
-const defaultColDef={sortable:true,editable:true,flex:1,filter:true,floatingFilter:true}
-const onGridReady=(params)=>{
-  gridApi=params.api
+
+const defaultColDef={
+  sortable:true,
+  editable:true,
+  flex:1,filter:true,
+  floatingFilter:true
 }
-const onExportClick=()=>{
-  gridApi.exportDataAsCsv();
-}
+
   return (
     <div className="App">
       <h1 align="center">React-App</h1>
       <h3>Student Details</h3>
-      <button onClick={()=>onExportClick()}>Export</button>
-      <div className="ag-theme-alpine" style={ {height: '200px'} }>
+      <div className="ag-theme-alpine" style={ {height: '400px'} }>
         <AgGridReact
             columnDefs={columnDefs}
             rowData={rowData}
             defaultColDef={defaultColDef}
-            onGridReady={onGridReady}>
+            enableBrowserTooltips={true} 
+             tooltipShowDelay={{tooltipShowDelay:0}}>
         </AgGridReact>
       </div>
     </div>
