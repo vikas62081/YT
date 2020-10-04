84d6f605c3b67849e34782f85196c3778fabdeb3
vikas62081 <vikas62081@gmail.com>
2020-10-04 17:20:47 +0530

    row selection in agGrid

--- src/App.js
+++ src/App.js
@@ -5,40 +5,54 @@ import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
 
 function App() {
-  const columnDefs= [
-    { headerName: "ID", field: "id" },
-    { headerName: "Name", field: "name",}, 
-    {headerName: "Email",field: "email",},
+  const columnDefs = [
+    { headerName: "ID", field: "id",checkboxSelection:true,headerCheckboxSelection:true },
+    { headerName: "Name", field: "name", },
+    { headerName: "Email", field: "email", },
     { headerName: "Body", field: "body" },
-    ]
-  const rowData= [
-    { name: "Rahul", age: 19, phoneNumber: 9876543210, birthYear:2001}, 
-    { name: "David", age: 17, phoneNumber: 9827654310,birthYear:2003}, 
-    { name: "Dan", age: 25, phoneNumber: 9765438210,birthYear:1995 }]
+  ]
 
-const defaultColDef={
-  sortable:true,
-  editable:true,
-  flex:1,filter:true,
-  floatingFilter:true
-}
+  const defaultColDef = {
+    sortable: true,
+    editable: true,
+    flex: 1, filter: true,
+    floatingFilter: true
+  }
 
-const onGridReady=(params)=>{
-  console.log("grid is ready")
-  fetch("https://jsonplaceholder.typicode.com/comments").then(resp=>resp.json())
-  .then(resp=>{console.log(resp)
-    params.api.applyTransaction({add:resp})})
-}
+  const onGridReady = (params) => {
+    console.log("grid is ready")
+    fetch("https://jsonplaceholder.typicode.com/comments").then(resp => resp.json())
+      .then(resp => {
+        console.log(resp)
+        params.api.applyTransaction({ add: resp }) //adding API data to grid
+      })
+  }
+  //define selection type single or multiple
+  const rowSelectionType='multiple'
+
+  //function will trigger once selection changed
+  const onSelectionChanged=(event)=>{
+    console.log(event.api.getSelectedRows())
+  }
+
+  //function to allow selection to row
+  const isRowSelectable=(node)=>{
+    return node.data?(node.data.id%2===0|| node.data.email.includes('.org')):false 
+  }
   return (
     <div className="App">
       <h1 align="center">React-App</h1>
       <h3>Student Details</h3>
-      <div className="ag-theme-alpine" style={ {height: '400px'} }>
+      <div className="ag-theme-alpine" style={{ height: '400px' }}>
         <AgGridReact
-            columnDefs={columnDefs}
-            // rowData={rowData}
-            defaultColDef={defaultColDef}
-            onGridReady={onGridReady}>
+          columnDefs={columnDefs}
+          defaultColDef={defaultColDef}
+          onGridReady={onGridReady}
+          rowSelection={rowSelectionType}
+          onSelectionChanged={onSelectionChanged}
+          rowMultiSelectWithClick={true}
+          isRowSelectable={isRowSelectable}
+          >
         </AgGridReact>
       </div>
     </div>
