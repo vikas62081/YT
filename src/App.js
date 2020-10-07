1dca9db6a3d205666dcfb058f41e3f5ef1559197
vikas62081 <vikas62081@gmail.com>
2020-10-07 14:47:14 +0530

    initial pagination commit

--- src/App.js
+++ src/App.js
@@ -3,42 +3,32 @@ import './App.css';
 import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
-
+import  RowGroupingModule  from 'ag-grid-community';
 function App() {
   const columnDefs = [
-    { headerName: "ID", field: "id",checkboxSelection:true,headerCheckboxSelection:true },
-    { headerName: "Name", field: "name", },
+    { headerName: "ID", field: "id",checkboxSelection:true,headerCheckboxSelection:true,},
+    { headerName: "Name", field: "name",},
     { headerName: "Email", field: "email", },
     { headerName: "Body", field: "body" },
-  ]
+    
+  ]  
 
   const defaultColDef = {
     sortable: true,
     editable: true,
     flex: 1, filter: true,
-    floatingFilter: true
+    floatingFilter: true, 
   }
 
   const onGridReady = (params) => {
-    console.log("grid is ready")
     fetch("https://jsonplaceholder.typicode.com/comments").then(resp => resp.json())
       .then(resp => {
-        console.log(resp)
         params.api.applyTransaction({ add: resp }) //adding API data to grid
+        params.api.paginationGoToPage(4)
       })
+     
   }
-  //define selection type single or multiple
-  const rowSelectionType='multiple'
-
-  //function will trigger once selection changed
-  const onSelectionChanged=(event)=>{
-    console.log(event.api.getSelectedRows())
-  }
-
-  //function to allow selection to row
-  const isRowSelectable=(node)=>{
-    return node.data?(node.data.id%2===0|| node.data.email.includes('.org')):false 
-  }
+ 
   return (
     <div className="App">
       <h1 align="center">React-App</h1>
@@ -48,10 +38,10 @@ function App() {
           columnDefs={columnDefs}
           defaultColDef={defaultColDef}
           onGridReady={onGridReady}
-          rowSelection={rowSelectionType}
-          onSelectionChanged={onSelectionChanged}
-          rowMultiSelectWithClick={true}
-          isRowSelectable={isRowSelectable}
+          pagination={true}
+          // paginationAutoPageSize={true}
+          // suppressPaginationPanel
+          paginationPageSize={5}
           >
         </AgGridReact>
       </div>
