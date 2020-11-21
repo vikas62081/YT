9a66329de2b741f855f7ab6cc904e120d87283c8
vikas62081 <vikas62081@gmail.com>
2020-11-21 22:11:24 +0530

    commiting api call in class component changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {posts:[]};
+  }
 
-  return (
-    <div className="App">
-      <h1 align="center">React-App</h1>
-      
-    </div>
-  );
+  //this lifecycle methid will get called only once
+  // when app rendered successfully
+  componentDidMount(){
+    //api call
+    fetch('https://jsonplaceholder.typicode.com/posts').then(resp=>resp.json())
+    .then(resp=>this.setState({posts:resp}))
+  }
+  render() {
+    return (
+      <div >
+      <h1 align='center'>React App</h1>
+      {this.state.posts.map(post=><div key={post.id}>{post.id}.{post.title}</div>)}
+      </div>
+    );
+  }
 }
-
-export default App;
