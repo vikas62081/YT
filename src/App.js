cd7b6cfa723132e776ef9b99eedc7f749ee3fbd5
vikas62081 <vikas62081@gmail.com>
2020-12-01 23:14:16 +0530

    login page design pushed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import './App.css';
 import Login from './components/login';
-import { ThemeProvider } from '@material-ui/core';
-import {customTheme} from './components/customTheme'
 function App() {
-
   return (
     <div className="App">
-      <ThemeProvider theme={customTheme}>
       <Login />
-      </ThemeProvider>
     </div>
   );
 }
--- src/components/login.js
+++ src/components/login.js
@@ -1,28 +1,45 @@
 import React from 'react'
-import { Grid, Paper, TextField, Typography, Button } from '@material-ui/core'
+import { Grid,Paper, Avatar, TextField, Button, Typography,Link } from '@material-ui/core'
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
-const Login = () => {
-  const paperStyle = { padding: 30, height: '70vh', maxWidth: 280, margin: "0 auto" }
-  return <Grid>
+const Login=()=>{
 
-    <Paper elevation={3} style={paperStyle}>
-      <h1 align='center'>React App</h1>
-      <Typography variant='h5'>Sign in</Typography>
-      <TextField label="Username" placeholder="Enter username" fullWidth required />
-      <TextField label="Password" placeholder="Enter password" type='password' fullWidth required />
-      <FormControlLabel
-        control={
-          <Checkbox
-            name="checkedB"
-            color="primary"
-          />
-        }
-        label="Remember me"
-      />
-      <Button variant='contained' style={{ margin: "10px 0" }} color='secondary' fullWidth>Sign in</Button>
-    </Paper>
-  </Grid>
+    const paperStyle={padding :20,height:'70vh',width:280, margin:"20px auto"}
+    const avatarStyle={backgroundColor:'#1bbd7e'}
+    const btnstyle={margin:'8px 0'}
+    return(
+        <Grid>
+            <Paper elevation={10} style={paperStyle}>
+                <Grid align='center'>
+                     <Avatar style={avatarStyle}><LockOutlinedIcon/></Avatar>
+                    <h2>Sign In</h2>
+                </Grid>
+                <TextField label='Username' placeholder='Enter username' fullWidth required/>
+                <TextField label='Password' placeholder='Enter password' type='password' fullWidth required/>
+                <FormControlLabel
+                    control={
+                    <Checkbox
+                        name="checkedB"
+                        color="primary"
+                    />
+                    }
+                    label="Remember me"
+                 />
+                <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
+                <Typography >
+                     <Link href="#" >
+                        Forgot password ?
+                </Link>
+                </Typography>
+                <Typography > Do you have an account ?
+                     <Link href="#" >
+                        Sign Up 
+                </Link>
+                </Typography>
+            </Paper>
+        </Grid>
+    )
 }
 
-export default Login;
+export default Login
