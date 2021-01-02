53a3ed0bdafc4abb58513f72fb03fc492d8ecddc
vikas62081 <vikas62081@gmail.com>
2021-01-02 09:46:34 +0530

    adding sign up form design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Login from './components/login';
+import Signup from './components/signup';
 function App() {
   return (
     <div className="App">
-      <Login />
+      {/* <Login /> */}
+      <Signup/>
     </div>
   );
 }
---
+++ src/components/signup.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Grid, Paper, Avatar, Typography, TextField, Button } from '@material-ui/core'
+import AddCircleOutlineOutlinedIcon from '@material-ui/icons/AddCircleOutlineOutlined';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormLabel from '@material-ui/core/FormLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+const Signup = () => {
+    const paperStyle = { padding: '30px 20px', width: 300, margin: "20px auto" }
+    const headerStyle = { margin: 0 }
+    const avatarStyle = { backgroundColor: '#1bbd7e' }
+    const marginTop = { marginTop: 5 }
+    return (
+        <Grid>
+            <Paper elevation={20} style={paperStyle}>
+                <Grid align='center'>
+                    <Avatar style={avatarStyle}>
+                        <AddCircleOutlineOutlinedIcon />
+                    </Avatar>
+                    <h2 style={headerStyle}>Sign Up</h2>
+                    <Typography variant='caption' gutterBottom>Please fill this form to create an account !</Typography>
+                </Grid>
+                <form>
+                    <TextField fullWidth label='Name' placeholder="Enter your name" />
+                    <TextField fullWidth label='Email' placeholder="Enter your email" />
+                    <FormControl component="fieldset" style={marginTop}>
+                        <FormLabel component="legend">Gender</FormLabel>
+                        <RadioGroup aria-label="gender" name="gender" style={{ display: 'initial' }}>
+                            <FormControlLabel value="female" control={<Radio />} label="Female" />
+                            <FormControlLabel value="male" control={<Radio />} label="Male" />
+                        </RadioGroup>
+                    </FormControl>
+                    <TextField fullWidth label='Phone Number' placeholder="Enter your phone number" />
+                    <TextField fullWidth label='Password' placeholder="Enter your password"/>
+                    <TextField fullWidth label='Confirm Password' placeholder="Confirm your password"/>
+                    <FormControlLabel
+                        control={<Checkbox name="checkedA" />}
+                        label="I accept the terms and conditions."
+                    />
+                    <Button type='submit' variant='contained' color='primary'>Sign up</Button>
+                </form>
+            </Paper>
+        </Grid>
+    )
+}
+
+export default Signup;
