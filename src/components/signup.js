03fc5f530dd1ff39e68327ef27831519095bd9a1
vikas62081 <vikas62081@gmail.com>
2021-01-09 14:33:33 +0530

    sign up validation done

--- src/components/signup.js
+++ src/components/signup.js
@@ -7,11 +7,41 @@ import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import Checkbox from '@material-ui/core/Checkbox';
+import { Formik, Field, Form, ErrorMessage } from 'formik'
+import { FormHelperText } from '@material-ui/core'
+import * as Yup from 'yup'
 const Signup = () => {
     const paperStyle = { padding: 20, width: 300, margin: "0 auto" }
     const headerStyle = { margin: 0 }
     const avatarStyle = { backgroundColor: '#1bbd7e' }
     const marginTop = { marginTop: 5 }
+    const initialValues = {
+        name: '',
+        email: '',
+        gender: '',
+        phoneNumber: '',
+        password: '',
+        confirmPassword: '',
+        termsAndConditions: false
+    }
+    const validationSchema = Yup.object().shape({
+        name: Yup.string().min(3, "It's too short").required("Required"),
+        email: Yup.string().email("Enter valid email").required("Required"),
+        gender: Yup.string().oneOf(["male", "female"], "Required").required("Required"),
+        phoneNumber: Yup.number().typeError("Enter valid Phone Number").required('Required'),
+        password: Yup.string().min(8, "Password minimum length should be 8").required("Required"),
+        confirmPassword: Yup.string().oneOf([Yup.ref('password')], "Password not matched").required("Required"),
+        termsAndConditions: Yup.string().oneOf(["true"], "Accept terms & conditions")
+    })
+    const onSubmit = (values, props) => {
+        console.log(values)
+        console.log(props)
+        setTimeout(() => {
+
+            props.resetForm()
+            props.setSubmitting(false)
+        }, 2000)
+    }
     return (
         <Grid>
             <Paper style={paperStyle}>
@@ -22,25 +52,41 @@ const Signup = () => {
                     <h2 style={headerStyle}>Sign Up</h2>
                     <Typography variant='caption' gutterBottom>Please fill this form to create an account !</Typography>
                 </Grid>
-                <form>
-                    <TextField fullWidth label='Name' placeholder="Enter your name" />
-                    <TextField fullWidth label='Email' placeholder="Enter your email" />
-                    <FormControl component="fieldset" style={marginTop}>
-                        <FormLabel component="legend">Gender</FormLabel>
-                        <RadioGroup aria-label="gender" name="gender" style={{ display: 'initial' }}>
-                            <FormControlLabel value="female" control={<Radio />} label="Female" />
-                            <FormControlLabel value="male" control={<Radio />} label="Male" />
-                        </RadioGroup>
-                    </FormControl>
-                    <TextField fullWidth label='Phone Number' placeholder="Enter your phone number" />
-                    <TextField fullWidth label='Password' placeholder="Enter your password"/>
-                    <TextField fullWidth label='Confirm Password' placeholder="Confirm your password"/>
-                    <FormControlLabel
-                        control={<Checkbox name="checkedA" />}
-                        label="I accept the terms and conditions."
-                    />
-                    <Button type='submit' variant='contained' color='primary'>Sign up</Button>
-                </form>
+                <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+                    {(props) => (
+                        <Form>
+
+                            <Field as={TextField} fullWidth name="name" label='Name'
+                                placeholder="Enter your name" helperText={<ErrorMessage name="name" />} />
+                            <Field as={TextField} fullWidth name="email" label='Email'
+                                placeholder="Enter your email" helperText={<ErrorMessage name="email" />} />
+                            <FormControl component="fieldset" style={marginTop}>
+                                <FormLabel component="legend">Gender</FormLabel>
+                                < Field as={RadioGroup} aria-label="gender" name="gender" name="gender" style={{ display: 'initial' }}>
+                                    <FormControlLabel value="female" control={<Radio />} label="Female" />
+                                    <FormControlLabel value="male" control={<Radio />} label="Male" />
+                                </ Field>
+                            </FormControl>
+                            <FormHelperText><ErrorMessage name="gender" /></FormHelperText>
+                            <Field as={TextField} fullWidth name="phoneNumber" label='Phone Number'
+                                placeholder="Enter your phone number" helperText={<ErrorMessage name="phoneNumber" />} />
+                            <Field as={TextField} fullWidth name='password' type="password"
+                                label='Password' placeholder="Enter your password"
+                                helperText={<ErrorMessage name="password" />} />
+                            <Field as={TextField} fullWidth name="confirmPassword" type="password"
+                                label='Confirm Password' placeholder="Confirm your password"
+                                helperText={<ErrorMessage name="confirmPassword" />} />
+                            <FormControlLabel
+                                control={<Field as={Checkbox} name="termsAndConditions" />}
+                                label="I accept the terms and conditions."
+                            />
+                            <FormHelperText><ErrorMessage name="termsAndConditions" /></FormHelperText>
+                            <Button type='submit' variant='contained' disabled={props.isSubmitting}
+                                color='primary'>{props.isSubmitting ? "Loading" : "Sign up"}</Button>
+
+                        </Form>
+                    )}
+                </Formik>
             </Paper>
         </Grid>
     )
